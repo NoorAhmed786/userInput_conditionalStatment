86726731b9114de41bd92077ce9225b095dc23e1
NoorAhmed786 <aniiahmed380@gmail.com>
2023-05-15 22:44:13 +0500

    prompt

---
+++ app.js
@@ -0,0 +1,138 @@
+// //qustn 1
+// // var userInput=prompt("enter a city name");
+// // if (userInput === "karachi"){
+// //     alert("welcome to city of lights");
+
+// // }
+
+
+// //qustn 2
+
+// userInput=prompt("TELL ME YOUR GENDER");
+// if(userInput === "male"){
+//     alert("welcome to sir");
+
+// }
+// else if(userInput === "female"){
+//     alert("welcome to mam");
+// }
+
+// //qustn 3
+// var userInput=prompt("enter a city name");
+// if (userInput.toUpperCase === "red".toUpperCase){
+//     document.write("<table border=3><tr><th>signal color</th><th>messages</th></tr> <tr><td>red</td><td> stop</td></tr><tr><td>yellow</td><td>ready to move</td></tr><tr><td>green</td><td>go</td></tr>");
+
+
+// }
+
+//qustn 4
+
+// userInput=parseInt(prompt("TELL Me REMAINIG FUEL"));
+// if(userInput < 0.25){
+//     alert("please refill the fuel in your car");
+
+// }
+// //qustn 5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+// //qustn 7
+// var guessNum=parseInt(prompt("GUESS A NUMBER"));
+// if(guessNum === 5){
+//     alert("BINGO!"+"correct ansewr!" );
+// }
+
+// var guessNum=parseInt(prompt("GUESS A NUMBER"));
+// if(guessNum === 5){
+//     alert("close enough to guess no");
+// }
+
+// //qustn 9
+// var guessNum=parseInt(prompt("GUESS A NUMBER"));
+// if (guessNum % 2 == 0){
+//     alert("the number is even");
+// }else{
+//     alert("the number is odd");
+// }
+
+// //qustn 10
+//  var userInput=parseInt(prompt("tell me a temperature or your site"));
+//  if(userInput === 40){
+//     alert("It is too hot outside.");
+//  }
+//  else if(userInput === 30){
+//     alert("the Weather today is Normal.");
+//  }
+//  else if(userInput === 20){
+//     alert("today’s Weather is cool.")
+//  }
+//  else if(userInput === 10){
+//     alert("OMG! Today’s weather is so Cool.");
+//  }
+
+ //qustn 11
+//  var operator = prompt('Enter operator to perform the calculation ( either +, -, * or / ): ');
+//  var num1=parseInt(prompt("enter a number to calculate"));
+//  var num2=parseInt(prompt("enter a number to calculate"));
+
+//  if(operator === "+"){
+//     alert(result=num1+num2);
+//  }
+//  else if(operator === "-"){
+//    alert(result=num1-num2);
+//  } else if( operator === "/"){
+//     alert(result=num1/num2);
+//  }else if(operator === "*"){
+//    alert(result=num1*num2);
+//  }
+
+ //qustn 8
+ var number = parseInt(prompt("enter a number"));
+  if( number % 3 == 0){
+    alert("the number is divisible by 3 do somthing else");
+  }
+  else {
+    alert("the number is  not divisible by 3");
+  } 
+
+
+
+
+
+
+    
+
+    
